386cf73f66f1e8861c1314de4cdb6b58ff8b1d55
GhadaAhmed152420 <74376575+GhadaAhmed152420@users.noreply.github.com>
2022-04-23 16:33:55 +0200

    finish login screen

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:wallet/views/register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_forward,
+                color: Colors.black,
+              )),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/mail.png",
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "أهلا بك من جديد!",
+              style: TextStyle(fontSize: 24),
+              textDirection: TextDirection.rtl,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('البريد الالكترونى'),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'ادخل/ي البريد الالكترونى',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text('كلمه المرور'),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'على الاقل ٦ أحرف ',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent.shade400,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              child: MaterialButton(
+                onPressed: () {
+
+                },
+                child: const Text(
+                  'تسجيل الدخول',
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()));
+              },
+              child: const Text(
+                'هل نسيت كلمه السر؟',
+              ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RegisterScreen()));
+                  },
+                  child: const Text(
+                    'تسجيل الاشتراك',
+                  ),
+                ),
+                const Text(
+                  'لا تملك حسابا؟',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
