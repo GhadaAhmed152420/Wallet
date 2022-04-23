155cb130cc6e45102c9e128b7e1233ad6a1223a5
GhadaAhmed152420 <74376575+GhadaAhmed152420@users.noreply.github.com>
2022-04-23 16:19:15 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Noto Sans',
+        appBarTheme: const AppBarTheme(
+          elevation: 0.0,
+        ),
+      ),
+
+    );
+  }
+}
