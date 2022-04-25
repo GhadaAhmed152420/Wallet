2c9f31497ed4fa3101a5a14eefd43083300c13a8
GhadaAhmed152420 <74376575+GhadaAhmed152420@users.noreply.github.com>
2022-04-25 19:39:22 +0200

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wallet/views/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.light,
     ),
   );
+
   runApp(const MyApp());
 }
 
